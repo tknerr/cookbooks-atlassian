a2eb38dad26242e5db7d940b389aa1a9d9cb06b9
tkn <tkn@zuehlke.com>
2012-09-27 15:07:13 +0200

    initial cookboko structure

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "tkn"
+maintainer_email "tkn@zuehlke.com"
+license          "All rights reserved"
+description      "Installs/Configures atlassian"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+name             "atlassian"
