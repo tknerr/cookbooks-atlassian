9f20d5b47751c476e412da0781a8841364ea7868
tkn <tkn@zuehlke.com>
2012-09-28 16:10:35 +0200

    fix service startup issue #1: JAVA_HOME is not found

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,7 +8,14 @@
 #
 
 include_recipe "ark"
-include_recipe "java"
+include_recipe "java::openjdk"
 include_recipe "runit"
+
+# XXX: fix missing symlink on ubuntu - actually a bug in the java recipe?
+if platform?("ubuntu", "debian") and node['java']['jdk_version'].to_i == 6
+  link node['java']['java_home'] do
+    to "/usr/lib/jvm/java-6-openjdk-amd64/"
+  end
+end
 
 
